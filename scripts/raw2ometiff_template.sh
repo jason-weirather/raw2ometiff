052f40e967db2659f9ae44392ecb4a836a9483d1
Jason Weirather <jason-weirather@users.noreply.github.com>
2023-04-11 12:35:12 -0400

    make script build more universal

--- scripts/raw2ometiff_template.sh
+++ scripts/raw2ometiff_template.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 JAR_DIR="$(dirname "$SCRIPT_DIR")"
-java -jar "$JAR_DIR/libs/raw2ometiff-unspecified.jar" "$@"
+JAR_FILE=$(find "$JAR_DIR/libs" -iname 'raw2ometiff*.jar' | head -n 1)
+java -jar "$JAR_FILE" "$@"
+
+
+#SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+#JAR_DIR="$(dirname "$SCRIPT_DIR")"
+#java -jar "$JAR_DIR/libs/raw2ometiff-unspecified.jar" "$@"
